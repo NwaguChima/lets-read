dc42665aaa505d940747fd063ea02a5e8a8041db
NwaguChima <nwaguchima48@gmail.com>
2022-09-28 07:43:39 +0100

    added new post builder

--- features/posts/postsSlice.js
+++ features/posts/postsSlice.js
@@ -7,6 +7,11 @@ export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
   return response.data;
 });
 
+export const addNewPost = createAsyncThunk("posts/addNewPost", async (post) => {
+  const response = await axios.post(POST_URL, post);
+  return response.data;
+});
+
 const initialState = {
   posts: [
     {
@@ -66,6 +71,9 @@ export const postsSlice = createSlice({
       .addCase(fetchPosts.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error.message;
+      })
+      .addCase(addNewPost.fulfilled, (state, action) => {
+        state.posts.push(action.payload);
       });
   },
 });
