8403c092dcb5a3f3c184dcee3b8909791c210dce
NwaguChima <nwaguchima48@gmail.com>
2022-09-28 07:40:18 +0100

    fetched user

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import "../styles/globals.css";
 import { store } from "../app/store";
 import { Provider } from "react-redux";
+import { fetchUsers } from "../features/users/usersSlice";
+
+// this is done to ensure users are gotten before the page is rendered
+store.dispatch(fetchUsers());
 
 function MyApp({ Component, pageProps }) {
   return (
